ea4549522313ef12ffb2edb884dc0cc684750c33
Iana Sanduta <iana.sandutsa@gmail.com>
2021-10-27 11:58:56 -0600

    Template

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const path = require('path')
 function createWindow () {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 800,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
@@ -43,3 +43,7 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+
+
